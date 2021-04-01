acb771db6dc9b465cb24085e0ed67b0365432138
Hiroki Nakano <30650175+SRK400KRZ@users.noreply.github.com>
2021-04-01 09:13:17 -0400

    can store posts into pandas dataframe

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from pprint import pprint
+
+import pandas as pd
+
+
 import requests.auth
 client_auth = requests.auth.HTTPBasicAuth('rcZ8hipjewj8Rg', 'R8y2HYAWUdYDtwjw6hWZu8FLVC9-dw')
 post_data = {"grant_type": "password", "username": "Right_Drop_2757", "password": "hanover1!"}
@@ -11,6 +16,20 @@ oneTimeToken = response.json()
 # 'scope': '*'}
 
 headers = {"Authorization": "bearer " + oneTimeToken["access_token"], "User-Agent": "Crawler by Right_Drop_2757"}
-response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
-myinfo = response.json()
-print(myinfo)
+response = requests.get("https://oauth.reddit.com/r/compsci", headers=headers, params={"limit": 100})
+df = pd.DataFrame()  # initialize dataframe
+for post in response.json()['data']['children']:
+    df = df.append({
+        'subreddit': post['data']['subreddit'],
+        'title': post['data']['title'],
+        'selftext': post['data']['selftext'],
+        'upvote_ratio': post['data']['upvote_ratio'],
+        'ups': post['data']['ups'],
+        'downs': post['data']['downs'],
+        'score': post['data']['score']
+    }, ignore_index=True)
+    pprint(post['data']['selftext'])
+
+print(len(df))
+
+
