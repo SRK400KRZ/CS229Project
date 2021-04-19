626c2c59fb62d356f85987e2f53a5f81a0895487
Hiroki Nakano <30650175+SRK400KRZ@users.noreply.github.com>
2021-04-19 16:43:28 -0400

    add second clustering

--- visualizing.py
+++ visualizing.py
@@ -14,14 +14,30 @@ with open("cosDist.binaryfile", "rb") as f:
 numClust = 20
 group = fcluster(cosDict, t=numClust, criterion='maxclust')
 key, count = np.unique(group, return_counts=True)
-for k,c in zip(key, count):
+for k, c in zip(key, count):
     print(k, c)
 
 labels = list(group.tolist())
 with open("result.txt", "a") as f:
     for i in range(1, numClust + 1):
-        f.write("\n" + "Label {}, Count {}".format(i, count[i-1]))
+        f.write("\n" + "Label {}, Count {}".format(i, count[i - 1]))
         f.write("\n" + texts[labels.index(i)] + "\n")
         print("label {} done".format(i))
 
 # try to do the same process with label 9
+
+with open("labelNineTexts.txt", "wb") as f:
+    texts_nine = []
+    for i in range(len(labels)):
+        if labels[i] == 9:
+            texts_nine.append(texts[i])
+    pickle.dump(texts_nine, f)
+
+with open("subNine.txt", "wb") as f:
+    sub_nine = []
+    for i in range(len(labels)):
+        if labels[i] == 9:
+            sub_nine.append(subreddits[i])
+    pickle.dump(sub_nine, f)
+print("end subnine")
+
