09142b0421efce065802f6da1dc4bd333c9fd9f9
Hiroki Nakano <30650175+SRK400KRZ@users.noreply.github.com>
2021-04-19 21:49:29 -0400

    export word clouds

--- visualizing.py
+++ visualizing.py
@@ -1,8 +1,9 @@
 import pickle
-from pprint import pprint
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.cluster.hierarchy import fcluster
+from wordcloud import WordCloud
 
 with open("texts.txt", "rb") as f:
     texts = pickle.load(f)
@@ -11,33 +12,33 @@ with open("subreddits.txt", "rb") as f:
 with open("cosDist.binaryfile", "rb") as f:
     cosDict = pickle.load(f)
 
-numClust = 20
+numClust = 5
 group = fcluster(cosDict, t=numClust, criterion='maxclust')
 key, count = np.unique(group, return_counts=True)
 for k, c in zip(key, count):
     print(k, c)
 
 labels = list(group.tolist())
+with open("result.txt", "w") as f:
+    pass
+#     to clear previous result
+
 with open("result.txt", "a") as f:
     for i in range(1, numClust + 1):
         f.write("\n" + "Label {}, Count {}".format(i, count[i - 1]))
         f.write("\n" + texts[labels.index(i)] + "\n")
         print("label {} done".format(i))
 
-# try to do the same process with label 9
-
-with open("labelNineTexts.txt", "wb") as f:
-    texts_nine = []
-    for i in range(len(labels)):
-        if labels[i] == 9:
-            texts_nine.append(texts[i])
-    pickle.dump(texts_nine, f)
-
-with open("subNine.txt", "wb") as f:
-    sub_nine = []
-    for i in range(len(labels)):
-        if labels[i] == 9:
-            sub_nine.append(subreddits[i])
-    pickle.dump(sub_nine, f)
-print("end subnine")
-
+for k in key:
+    # get list of index of key
+    index_num = [n for n, v in enumerate(labels) if v == k]
+    # if len(index_num) > 5:
+    #     # avoid redundant accumulation
+    #     index_num = index_num[:5]
+    text = ""
+    for i in index_num:
+        text += texts[i]
+    wc = WordCloud(max_words=30).generate(text)
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
